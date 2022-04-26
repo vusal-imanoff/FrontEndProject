8cfc5cd4929aa6bc22d8efcd12c738be880fbe87
vusal-imanoff <vusalmi@code.edu.az>
2022-04-27 03:28:44 +0400

    create login and contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,8 @@ span.onclick = function () {
 $(document).ready(function () {
   $("#myModal").click(function (event) {
     if (event.target == modal) {
-      modal.style.display = "none";}
+      modal.style.display = "none";
+    }
   });
 });
 
@@ -107,65 +108,179 @@ $(document).ready(function () {
   });
 });
 
+//shop.css
+
+$(document).ready(function () {
+  $(".icon_beverages").click(function (event) {
+    event.preventDefault();
+    $(".icon_beverages").addClass('d-none');
+    $(".icon_beverages_minus").removeClass('d-none');
+  });
+
+  $(".icon_beverages_minus").click(function (event) {
+    event.preventDefault();
+    $(".icon_beverages").removeClass('d-none');
+    $(".icon_beverages_minus").addClass('d-none');
+  });
+
+  $(".icon_beverages_minus").click(function (event) {
+    event.preventDefault();
+    $(".ul_beverages").slideToggle();
+  });
+
+  $(".icon_beverages").click(function (event) {
+    event.preventDefault();
+    $(".ul_beverages").slideToggle();
+  });
+
+  $(".icon_breads").click(function (event) {
+    event.preventDefault();
+    $(".ul_breads").slideToggle();
+  });
+
+  $(".icon_breads").click(function (event) {
+    event.preventDefault();
+    $(".icon_breads").addClass('d-none');
+    $(".icon_breads_minus").removeClass('d-none');
+  });
+
+  $(".icon_breads_minus").click(function (event) {
+    event.preventDefault();
+    $(".icon_breads").removeClass('d-none');
+    $(".icon_breads_minus").addClass('d-none');
+  });
+
+  $(".icon_breads_minus").click(function (event) {
+    event.preventDefault();
+    $(".ul_breads").slideToggle();
+  });
+
+  $(".icon_breakfast").click(function (event) {
+    event.preventDefault();
+    $(".ul_breakfast").slideToggle();
+  });
+
+  $(".icon_breakfast").click(function (event) {
+    event.preventDefault();
+    $(".icon_breakfast").addClass('d-none');
+    $(".icon_breakfast_minus").removeClass('d-none');
+  });
+
+  $(".icon_breakfast_minus").click(function (event) {
+    event.preventDefault();
+    $(".icon_breakfast").removeClass('d-none');
+    $(".icon_breakfast_minus").addClass('d-none');
+  });
+
+  $(".icon_breakfast_minus").click(function (event) {
+    event.preventDefault();
+    $(".ul_breakfast").slideToggle();
+  });
+
+
+  $(".icon_fruits").click(function (event) {
+    event.preventDefault();
+    $(".ul_fruits").slideToggle();
+  });
+  $(".icon_fruits").click(function (event) {
+    event.preventDefault();
+    $(".icon_fruits").addClass('d-none');
+    $(".icon_fruits_minus").removeClass('d-none');
+  });
+
+  $(".icon_fruits_minus").click(function (event) {
+    event.preventDefault();
+    $(".icon_fruits").removeClass('d-none');
+    $(".icon_fruits_minus").addClass('d-none');
+  });
+
+  $(".icon_fruits_minus").click(function (event) {
+    event.preventDefault();
+    $(".ul_fruits").slideToggle();
+  });
+  $(".icon_meats").click(function (event) {
+    event.preventDefault();
+    $(".ul_meats").slideToggle();
+  });
+  $(".icon_meats").click(function (event) {
+    event.preventDefault();
+    $(".icon_meats").addClass('d-none');
+    $(".icon_meats_minus").removeClass('d-none');
+  });
+
+  $(".icon_meats_minus").click(function (event) {
+    event.preventDefault();
+    $(".icon_meats").removeClass('d-none');
+    $(".icon_meats_minus").addClass('d-none');
+  });
+
+  $(".icon_meats_minus").click(function (event) {
+    event.preventDefault();
+    $(".ul_meats").slideToggle();
+  });
+
+});
+
+
 $(document).ready(function () {
   $("#open_menu").click(function () {
-    $(".header_menu").css('opacity', '1')
-    $(".header_menu").css('visibility', 'visible')
-    $(".header_menu").css('width', '100%')
+    $(".header_menu").css('display','block')
+    $(".header_menu").addClass('animate__fadeInLeft')
+    $(".header_menu").removeClass('animate__fadeOutLeft')
+
   });
 
   $("#close_menu").click(function (event) {
     event.preventDefault();
-    $(".header_menu").css('opacity', '0')
-    $(".header_menu").css('visibility', 'hidden')
-    $(".header_menu").css('width', '0')
+    $(".header_menu").addClass('animate__fadeOutLeft')
+    $(".header_menu").removeClass('animate__fadeInLeft')
+
+
   });
   $("#category_icon").click(function (event) {
     event.preventDefault();
-    $(".header_menu").css('opacity', '1')
-    $(".header_menu").css('visibility', 'visible')
-    $(".header_menu").css('width', '100%')
+    $(".header_menu").css('display','block')
+    $(".header_menu").addClass('animate__fadeInLeft')
+    $(".header_menu").removeClass('animate__fadeOutLeft')
   });
 
 });
 
 $('.sliderall').slick({
-    infinite: true,
-    speed: 1000,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 480,
-        settings: {
-          autoplay:false,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite:true
-        }
+  infinite: true,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 577,
+      settings: {
+        autoplay: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
       }
-    ]
-  });
-  $('.product_bottom').slick({
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite:true
-        }
+    }
+  ]
+});
+$('.product_bottom').slick({
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 577,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true
       }
-    ]
-  });
-  
-
+    }
+  ]
+});
 
 
 
--- assets/js/test.js
+++
@@ -1,4 +0,0 @@
-// $('#category_icon').on("click", function(e){
-//     e.preventdDefault()
-//     console.log("salam")
-//    })
